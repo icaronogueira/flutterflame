92b56fdeee1e835eb9adca78f02a03ac657155f8
icaro.nogueira@git.tjam.jus.br <icaro.nogueira@git.tjam.jus.br>
2022-07-19 17:49:15 -0400

    curso flame

--- itch/charlie_chicken/lib/main.dart
+++ itch/charlie_chicken/lib/main.dart
@@ -1,9 +1,25 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
+import 'package:flame/image_composition.dart';
+import 'package:flutter/material.dart' hide Image;
 
 void main() {
+  print("1. Load the GameWidget with the runApp");
   runApp(GameWidget(game: ChickenGame()));
 }
 
-class ChickenGame extends  {
+class ChickenGame extends FlameGame {
+
+  double chickenScaleFactor = 3.0;
+
+  @override
+  Future<void>? onLoad() async{
+    // TODO: implement onLoad
+    await super.onLoad();
+    print("2. load the assets for the game");
+    Image  chickenImage = await images.load("chicken.png");
+    SpriteComponent chickenSprite  = SpriteComponent.fromImage(chickenImage, srcSize: Vector2(32,34))..size = Vector2(32,34) * chickenScaleFactor;
+    add(chickenSprite);
+  }
+
 }
