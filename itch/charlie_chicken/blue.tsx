6672df1399ec89ab93c1b71c28d4ad1d7ad29be1
icaro.nogueira@git.tjam.jus.br <icaro.nogueira@git.tjam.jus.br>
2022-07-20 22:01:57 -0400

    tiled instalattion

---
+++ itch/charlie_chicken/blue.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="blue" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="assets/images/world/blue.png" width="64" height="64"/>
+</tileset>
--- itch/charlie_chicken/lib/main.dart
+++ itch/charlie_chicken/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/image_composition.dart';
+import 'package:flame/palette.dart';
 import 'package:flutter/material.dart' hide Image;
 
 void main() {
@@ -8,18 +9,60 @@ void main() {
   runApp(GameWidget(game: ChickenGame()));
 }
 
-class ChickenGame extends FlameGame {
+class ChickenGame extends FlameGame with HasDraggables {
 
-  double chickenScaleFactor = 3.0;
+  double chickenScaleFactor = 3.0; 
+  late SpriteAnimationComponent chicken;
+  late final JoystickComponent joystick;
+  bool chickenFlipped = false;
+  late SpriteComponent background; 
 
   @override
   Future<void>? onLoad() async{
     // TODO: implement onLoad
     await super.onLoad();
+    
+    background = SpriteComponent()..sprite = await loadSprite('background.png')
+    ..size = size;
+    add(background);
+
     print("2. load the assets for the game");
     Image  chickenImage = await images.load("chicken.png");
-    SpriteComponent chickenSprite  = SpriteComponent.fromImage(chickenImage, srcSize: Vector2(32,34))..size = Vector2(32,34) * chickenScaleFactor;
-    add(chickenSprite);
+    var chickenAnimation = SpriteAnimation.fromFrameData(chickenImage, SpriteAnimationData.sequenced(amount: 14, stepTime: 0.1, textureSize: Vector2(32,34)));
+    chicken = SpriteAnimationComponent()
+    ..animation = chickenAnimation
+    ..size = Vector2(32, 32) * chickenScaleFactor
+    ..position = Vector2(300,100);
+    add(chicken);
+
+    final knobPaint = BasicPalette.blue.withAlpha(200).paint();
+    final backgroundPaint = BasicPalette.blue.withAlpha(100).paint();
+
+    joystick = JoystickComponent(
+      knob: CircleComponent(radius: 30, paint: knobPaint),
+      background: CircleComponent(radius: 100, paint: backgroundPaint),
+      margin: const EdgeInsets.only(left: 40, bottom: 40));
+
+    add(joystick);
+
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+    super.update(dt);
+    // chicken.x -= 1;
+    // chicken.y += 1;
+    chicken.position.add(joystick.relativeDelta*300*dt);
+    if (joystick.relativeDelta[0]<0 && chickenFlipped) {
+      chickenFlipped = false;
+      chicken.flipHorizontallyAroundCenter();
+    } 
+    if (joystick.relativeDelta[0]>0 && !chickenFlipped){
+      chickenFlipped = true;
+      chicken.flipHorizontallyAroundCenter();
+    }
+    print(joystick.relativeDelta);
   }
 
 }
